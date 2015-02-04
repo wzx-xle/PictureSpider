4eedccfc51bf3debf6622156fd298d7843d7818a
wxyz <hyhjwzx@126.com>
2015-02-04 10:58:24 +0800

    添加web的http服务器

--- web/server.js
+++ web/server.js
@@ -1,2 +1,7 @@
 var http = require('http');
 
+http.createServer(function (req, resp) {
+    resp.writeHead('200', {'Content-Type': 'text/plain'});
+    resp.end('\nhello world!');
+}).listen(8088);
+console.log('Server running at http://localhost:8088/')
