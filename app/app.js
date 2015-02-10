baf664f4099cc4258accfaf2d9eb7ed908273619
wxyz <hyhjwzx@126.com>
2015-02-10 17:31:39 +0800

    1、增加读取当前目录的文件和读取文件内容的函数；
    2、客户端实现生成小组页访问url的方法；
    3、增加初始化cookie集合和切换cookie的函数

--- app/app.js
+++ app/app.js
@@ -11,16 +11,100 @@ var config = common.config;
 var groups = config.app.groups;
 // 小组URL模版
 var groupUrlTemplate = config.app.groupUrlTemplate;
-var groupReferer = config.app.groupReferer;
 var maxPage = config.app.maxPage;
 
+// cookies
+var cookies = [""];
+
+// 请求头参数
+var groupReferer = config.app.groupReferer;
+var currCookie = 0;
+
+/**
+ * 初始化访问的cookie列表
+ * @param {String} dir cookies的保存路径
+ */
+var initCookie = function (dir) {
+    var files = utils.getFilesSync(dir);
+    for (var i = 0; i < files.length; i++) {
+        var cookie = utils.readFileSync(files[i]);
+        if ("" != cookie) {
+            cookies.push(cookie);
+        }
+    }
+}
+
+/**
+ * 切换cookie
+ */
+var switchCookie = function () {
+    if (currCookie == cookies.length) {
+        currCookie = 0;
+    }
+    else {
+        currCookie++;
+    }
+}
+
+debugger;
 // 定时检查
-utils.timer(1000 * config.app.groups.length, function () {
+utils.timer(5 * 1000 * groups.length, function () {
+    var groupPageUrls = [];
+    for (var i = 0; i < groups.length; i++) {
+        var groupId = groups[i];
+        for (var j = 0; j < maxPage; j++) {
+            var url = groupUrlTemplate.replace('{1}', groupId).replace('{2}', j * 25);
+            groupPageUrls.push(url);
+        }
+    }
+    console.log(groupPageUrls);
+
+    var topics = [];
+    utils.timerFor(1000, groupPageUrls, function (groupUrl) {
+        superagent.get(url).end(function (err, sres) {
+            if (err) {
+                console.error(err);
+            }
+
+            var $ = cheerio.load(sres.text);
+            var result = $('.olt .title a');
+            if (result && result.length != 0) {
+                result.each(function (k, v) {
+                    topics.push({
+                        refer: groupUrl,
+                        url: v.attribs['href']
+                    });
+                });
+            }
+            else {
+                switchCookie();
+            }
+
+            console.log("topics:" + topics.length);
+        });
+    });
+
+    return;
 
     // 循环变量所有小组
     utils.timerFor(1000, config.app.groups, function (group) {
+        var topics = [];
+        var groupPages = [];
         for (var i = 0; i < maxPage; i++) {
-            var url = groupUrlTemplate.replace('{1}', group).replace('{2}', i * 50);
+            var url = groupUrlTemplate.replace('{1}', group).replace('{2}', i * 25);
+            console.log('url:' + url);
+            superagent.get(url).end(function (err, sres) {
+                if (err) {
+                    console.error(err);
+                }
+
+                var $ = cheerio.load(sres.text);
+                $('.olt .title a').each(function (k, v) {
+                    topics.push(v.attribs['href']);
+                });
+
+                console.log("topics:" + topics.length);
+            });
         }
     });
 });
--- cmn/utils.js
+++ cmn/utils.js
@@ -7,28 +7,56 @@ var utils = {};
  * 同步方式创建路径，包括不存在的父目录
  * @param {String}   dirPath  目录
  * @param {Number}   model    目录权限，默认0777
- * @param {Function} callback 回调函数
  */
-utils.mkdirsSync = function (dirPath, model, callback) {
-    try {
-        if (!path.existsSync(dirPath)) {
-            var parentDir = path.dirname(dirPath);
-            if (path.existsSync(parentDir)) {
+utils.mkdirsSync = function (dirPath, model) {
+    if (!fs.existsSync(dirPath)) {
+        var parentDir = path.dirname(dirPath);
+        if (fs.existsSync(parentDir)) {
+            fs.mkdirSync(dirPath, model);
+        } else {
+            Utils.mkdirs(parentDir, model, function (err) {
                 fs.mkdirSync(dirPath, model);
-                callback();
-            } else {
-                Utils.mkdirs(parentDir, model, function (err) {
-                    if (err) callback(err);
-
-                    fs.mkdirSync(dirPath, model);
-                    callback();
-                });
-            }
+            });
         }
-    } catch (err) {
-        callback(err);
     }
 };
+
+/**
+ * 获取目录下的所有文件，不包含子目录
+ * @param {String} dir 目录路径
+ * @param {Array}  文件数组
+ */
+utils.getFilesSync = function (dir) {
+    if (!dir) {
+        dir = __dirname;
+    }
+
+    var res = [];
+    if (fs.existsSync(dir)) {
+        var files = fs.readdirSync(dir);
+        for (var i = 0; i < files.length; i++) {
+            if (fs.statSync(files).isFile()) {
+                res.push(files);
+            }
+        }
+    }
+
+    return res;
+}
+
+/**
+ * 读取文件内容
+ * @param   {String} filePath 文件路径
+ * @param   {Object} options  文件读取参数，默认为utf-8
+ * @returns {String} 文件内容，转换到字符串
+ */
+utils.readFileSync = function (filePath, options) {
+    if (!filePath || !fs.existsSync(filePath)) {
+        return "";
+    }
+
+    return fs.readFileSync(filePath, options);
+}
 
 /**
  * 定时器，在回调函数执行完成后，做下一次定时操作
@@ -54,8 +82,8 @@ var timeForIndex = 0;
  */
 utils.timerFor = function (time, params, callback) {
     var self = function () {
-        callback(params[timeForIndex++]);
         if (params.length > timeForIndex) {
+            callback(params[timeForIndex++]);
             setTimeout(self, time);
         }
     };
