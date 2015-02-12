3540c7153ce0efb829f2f089fcbfe3b0e873bb46
wxyz <hyhjwzx@126.com>
2015-02-12 14:51:47 +0800

    1、增加403错误页面和控制器

--- web/controller/error.js
+++ web/controller/error.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 
 module.exports = {
     e404: function (resp) {
@@ -6,6 +7,15 @@ module.exports = {
         });
 
         fs.readFile(__dirname + '/../www/view/err/404.html', function (err, data) {
+            resp.end(data);
+        });
+    },
+    e403: function (resp) {
+        resp.writeHead('403', {
+            'Content-Type': 'text/html'
+        });
+
+        fs.readFile(__dirname + '/../www/view/err/403.html', function (err, data) {
             resp.end(data);
         });
     }
